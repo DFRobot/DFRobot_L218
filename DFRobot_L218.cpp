1882d25247eb1dbfdb3f0c158a134159c817410d
dfrobot <ouki.wang@dfrobot.com>
2017-12-01 17:19:55 +0800

    updata cpp

--- DFRobot_L218.cpp
+++ DFRobot_L218.cpp
@@ -2,46 +2,46 @@
 
 bool  DFRobot_L218::initFile(void)
 {
-    if(!SIMcore.check_send_cmd("AT+FSCREATE=?\r\n","OK")){
-        SIMcore.setCommandCounter(0);
+    if(!check_send_cmd("AT+FSCREATE=?\r\n","OK")){
+        setCommandCounter(0);
         return false;
     }
     delay(10);
-    if(!SIMcore.check_send_cmd("AT+FSREAD=?\r\n","OK")){
-        SIMcore.setCommandCounter(0);
+    if(!check_send_cmd("AT+FSREAD=?\r\n","OK")){
+        setCommandCounter(0);
         return false;
     }
     delay(10);
-    if(!SIMcore.check_send_cmd("AT+FSWRITE=?\r\n","OK")){
-        SIMcore.setCommandCounter(0);
+    if(!check_send_cmd("AT+FSWRITE=?\r\n","OK")){
+        setCommandCounter(0);
         return false;
     }
     delay(10);
-    if(!SIMcore.check_send_cmd("AT+FSMKDIR=?\r\n","OK")){
-        SIMcore.setCommandCounter(0);
+    if(!check_send_cmd("AT+FSMKDIR=?\r\n","OK")){
+        setCommandCounter(0);
         return false;
     }
     delay(10);
-    if(!SIMcore.check_send_cmd("AT+FSRMDIR=?\r\n","OK")){
-        SIMcore.setCommandCounter(0);
+    if(!check_send_cmd("AT+FSRMDIR=?\r\n","OK")){
+        setCommandCounter(0);
         return false;
     }
     delay(10);
-    if(!SIMcore.check_send_cmd("AT+FSLS=?\r\n","OK")){
-        SIMcore.setCommandCounter(0);
+    if(!check_send_cmd("AT+FSLS=?\r\n","OK")){
+        setCommandCounter(0);
         return false;
     }
-    SIMcore.setCommandCounter(1);
+    setCommandCounter(1);
     return true;
 }
 
 char* DFRobot_L218::getList(void)
 {
-    if(SIMcore.getCommandCounter() == 1){
+    if(getCommandCounter() == 1){
         char SIMList[300];
-        SIMcore.cleanBuffer(SIMList,300);
-        SIMcore.send_cmd("AT+FSLS\r\n");
-        SIMcore.readBuffer(SIMList,300);
+        cleanBuffer(SIMList,300);
+        send_cmd("AT+FSLS\r\n");
+        readBuffer(SIMList,300);
         return SIMList+9;
     }else{
         return "error";
@@ -50,13 +50,13 @@ char* DFRobot_L218::getList(void)
 
 char* DFRobot_L218::getList(char *foldername)
 {
-    if(SIMcore.getCommandCounter() == 1){
+    if(getCommandCounter() == 1){
         char SIMList[300];
-        SIMcore.cleanBuffer(SIMList,300);
-        SIMcore.send_cmd("AT+FSLS=\"");
-        SIMcore.send_cmd(foldername);
-        SIMcore.send_cmd("\"\r\n");
-        SIMcore.readBuffer(SIMList,300);
+        cleanBuffer(SIMList,300);
+        send_cmd("AT+FSLS=\"");
+        send_cmd(foldername);
+        send_cmd("\"\r\n");
+        readBuffer(SIMList,300);
         return SIMList+8;
     }else{
         return "error";
@@ -65,11 +65,11 @@ char* DFRobot_L218::getList(char *foldername)
 
 char* DFRobot_L218::getSpace(void)
 {
-    if(SIMcore.getCommandCounter() == 1){
+    if(getCommandCounter() == 1){
         char fileSpace[50];
-        SIMcore.cleanBuffer(fileSpace,50);
-        SIMcore.send_cmd("AT+FSINFO=\"Z:\"\r\n");
-        SIMcore.readBuffer(fileSpace,50);
+        cleanBuffer(fileSpace,50);
+        send_cmd("AT+FSINFO=\"Z:\"\r\n");
+        readBuffer(fileSpace,50);
         return fileSpace+18;
     }else{
         return "error";
@@ -78,10 +78,10 @@ char* DFRobot_L218::getSpace(void)
 
 bool  DFRobot_L218::createFile(char *filename)
 {
-    if(SIMcore.getCommandCounter() == 1){
-        SIMcore.send_cmd("AT+FSCREATE=\"");
-        SIMcore.send_cmd(filename);
-        if(SIMcore.check_send_cmd("\"\r\n","OK")){
+    if(getCommandCounter() == 1){
+        send_cmd("AT+FSCREATE=\"");
+        send_cmd(filename);
+        if(check_send_cmd("\"\r\n","OK")){
             return true;
         }else{
             return false;
@@ -93,28 +93,28 @@ bool  DFRobot_L218::createFile(char *filename)
 
 char* DFRobot_L218::readFile(char *filename, int offset, int len, Mode mode)
 {
-    if(SIMcore.getCommandCounter() == 1){
+    if(getCommandCounter() == 1){
         int   rOffset = len+strlen(filename)+6;
         rOffset *=2;
         char  L218file[rOffset+50];
-        SIMcore.cleanBuffer(L218file,rOffset+50);
+        cleanBuffer(L218file,rOffset+50);
         char  num1[4],num2[4];
         itoa(offset, num1, 10);
         itoa(len,    num2, 10);
         if(mode == Normal){
-            SIMcore.send_cmd("AT+FSREAD=\"");
+            send_cmd("AT+FSREAD=\"");
         }else if(mode == Hex){
-            SIMcore.send_cmd("AT+FSREADHEX=\"");
+            send_cmd("AT+FSREADHEX=\"");
         }else{
             return "No such mode";
         }
-        SIMcore.send_cmd(filename);
-        SIMcore.send_cmd("\",");
-        SIMcore.send_cmd(num1);
-        SIMcore.send_cmd(",");
-        SIMcore.send_cmd(num2);
-        SIMcore.send_cmd("\r\n");
-        SIMcore.readBuffer(L218file,rOffset+50);
+        send_cmd(filename);
+        send_cmd("\",");
+        send_cmd(num1);
+        send_cmd(",");
+        send_cmd(num2);
+        send_cmd("\r\n");
+        readBuffer(L218file,rOffset+50);
         if(mode == Normal){
             return L218file+10;
         }
@@ -128,10 +128,10 @@ char* DFRobot_L218::readFile(char *filename, int offset, int len, Mode mode)
 
 bool  DFRobot_L218::deleteFile(char *filename)
 {
-    if(SIMcore.getCommandCounter() == 1){
-        SIMcore.send_cmd("AT+FSDEL=\"");
-        SIMcore.send_cmd(filename);
-        if(SIMcore.check_send_cmd("\"\r\n","OK")){
+    if(getCommandCounter() == 1){
+        send_cmd("AT+FSDEL=\"");
+        send_cmd(filename);
+        if(check_send_cmd("\"\r\n","OK")){
             return true;
         }else{
             return false;
@@ -143,22 +143,22 @@ bool  DFRobot_L218::deleteFile(char *filename)
 
 bool  DFRobot_L218::writeFile(char *filename, char *data, Mode mode)
 {
-    if(SIMcore.getCommandCounter() == 1){
+    if(getCommandCounter() == 1){
         char num[4];
         int len = strlen(data);
         itoa(len, num, 10);
         if(mode == Normal){
-            SIMcore.send_cmd("AT+FSWRITE=\"");
+            send_cmd("AT+FSWRITE=\"");
         }else if(mode == Hex){
-            SIMcore.send_cmd("AT+FSWRITEHEX=\"");
+            send_cmd("AT+FSWRITEHEX=\"");
         }else{
             return false;
         }
-        SIMcore.send_cmd(filename);
-        SIMcore.send_cmd("\",1,");
-        SIMcore.send_cmd(num);
-        if(SIMcore.check_send_cmd("\r\n",">")){
-            if(SIMcore.check_send_cmd(data,"OK")){
+        send_cmd(filename);
+        send_cmd("\",1,");
+        send_cmd(num);
+        if(check_send_cmd("\r\n",">")){
+            if(check_send_cmd(data,"OK")){
                 return true;
             }else{
                 return false;
@@ -173,10 +173,10 @@ bool  DFRobot_L218::writeFile(char *filename, char *data, Mode mode)
 
 bool  DFRobot_L218::createFolder(char *foldername)
 {
-    if(SIMcore.getCommandCounter() == 1){
-        SIMcore.send_cmd("AT+FSMKDIR=\"");
-        SIMcore.send_cmd(foldername);
-        if(SIMcore.check_send_cmd("\"\r\n","OK")){
+    if(getCommandCounter() == 1){
+        send_cmd("AT+FSMKDIR=\"");
+        send_cmd(foldername);
+        if(check_send_cmd("\"\r\n","OK")){
             return true;
         }else{
             return false;
@@ -188,10 +188,10 @@ bool  DFRobot_L218::createFolder(char *foldername)
 
 bool  DFRobot_L218::deleteFolder(char *foldername)
 {
-    if(SIMcore.getCommandCounter() == 1){
-        SIMcore.send_cmd("AT+FSRMDIR=\"");
-        SIMcore.send_cmd(foldername);
-        if(SIMcore.check_send_cmd("\"\r\n","OK")){
+    if(getCommandCounter() == 1){
+        send_cmd("AT+FSRMDIR=\"");
+        send_cmd(foldername);
+        if(check_send_cmd("\"\r\n","OK")){
             return true;
         }else{
             return false;
@@ -205,7 +205,7 @@ bool   DFRobot_L218::initPos(void)
 {
     int  count = 0;
     while(count < 3){
-        if(SIMcore.check_send_cmd("AT+CGATT?\r\n","1")){
+        if(check_send_cmd("AT+CGATT?\r\n","1")){
             break;
         }else{
             count++;
@@ -213,19 +213,19 @@ bool   DFRobot_L218::initPos(void)
         }
     }
     if(count == 3){
-        SIMcore.closeCommand();
+        closeCommand();
         return false;
     }
-    if(SIMcore.check_send_cmd("AT+CSTT=\"CMNET\"\r\n","OK")){
-        if(SIMcore.check_send_cmd("AT+GTPOS=1\r\n","CONNECT OK")){
-            SIMcore.setCommandCounter(2);
+    if(check_send_cmd("AT+CSTT=\"CMNET\"\r\n","OK")){
+        if(check_send_cmd("AT+GTPOS=1\r\n","CONNECT OK")){
+            setCommandCounter(2);
             return true;
         }else{
-            SIMcore.closeCommand();
+            closeCommand();
             return false;
         }
     }else{
-        SIMcore.closeCommand();
+        closeCommand();
         return false;
     }
 }
@@ -234,73 +234,73 @@ bool   DFRobot_L218::getPosition(void)
 {
     char  posBuffer[80];
     char *pLongitude,*pLatitude;
-    SIMcore.cleanBuffer(posBuffer,80);
-    if((SIMcore.getCommandCounter() == 2) || (SIMcore.getCommandCounter() == 5)){
+    cleanBuffer(posBuffer,80);
+    if((getCommandCounter() == 2) || (getCommandCounter() == 5)){
 //        delay(500);
-        SIMcore.send_cmd("AT+GTPOS=2\r\n");
-        SIMcore.readBuffer(posBuffer,80);
+        send_cmd("AT+GTPOS=2\r\n");
+        readBuffer(posBuffer,80);
         if(NULL != strstr(posBuffer,"+GTPOS")){
             if(NULL != strstr(posBuffer,"$")){
-                SIMcore.setCommandCounter(3);
+                setCommandCounter(3);
             }else if(NULL != strstr(posBuffer,"-1")){
-                SIMcore.setCommandCounter(6);
+                setCommandCounter(6);
             }else if(NULL != strstr(posBuffer,"-2")){
-                SIMcore.setCommandCounter(7);
+                setCommandCounter(7);
             }else if(NULL != strstr(posBuffer,"-3")){
-                SIMcore.setCommandCounter(8);
+                setCommandCounter(8);
             }else if(NULL != strstr(posBuffer,"-4")){
-                SIMcore.setCommandCounter(9);
+                setCommandCounter(9);
             }else if(NULL != strstr(posBuffer,"-5")){
-                SIMcore.setCommandCounter(10);
+                setCommandCounter(10);
             }else if(NULL != strstr(posBuffer,"-6")){
-                SIMcore.setCommandCounter(11);
+                setCommandCounter(11);
             }else if(NULL != strstr(posBuffer,"-7")){
-                SIMcore.setCommandCounter(12);
+                setCommandCounter(12);
             }
         }else{
-            SIMcore.closeCommand();
+            closeCommand();
             return false;
         }
     }else{
-        SIMcore.closeCommand();
+        closeCommand();
         return false;
     }
-    if(SIMcore.getCommandCounter() == 3){
+    if(getCommandCounter() == 3){
 //Serial.println(posBuffer);
         pLongitude = strstr(posBuffer,":");
         pLatitude  = strstr(posBuffer,",");
         memcpy(longitude,pLongitude+2,7);
         memcpy(latitude, pLatitude+1 ,7);
-        SIMcore.setCommandCounter(4);
+        setCommandCounter(4);
         return true;
     }
 }
 
 char*   DFRobot_L218::getLongitude(void)
 {
-    if((SIMcore.getCommandCounter() == 4) || (SIMcore.getCommandCounter() == 5)){
-        SIMcore.setCommandCounter(5);
+    if((getCommandCounter() == 4) || (getCommandCounter() == 5)){
+        setCommandCounter(5);
         return longitude;
-    }else if(SIMcore.getCommandCounter() == 6 ){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 6 ){
+        setCommandCounter(2);
         return "Error:Network busy";
-    }else if(SIMcore.getCommandCounter() == 7 ){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 7 ){
+        setCommandCounter(2);
         return "Error:Not ready";
-    }else if(SIMcore.getCommandCounter() == 8 ){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 8 ){
+        setCommandCounter(2);
         return "Error:Network error";
-    }else if(SIMcore.getCommandCounter() == 9 ){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 9 ){
+        setCommandCounter(2);
         return "Error:Timeout";
-    }else if(SIMcore.getCommandCounter() == 10){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 10){
+        setCommandCounter(2);
         return "Error:Unack";
-    }else if(SIMcore.getCommandCounter() == 11){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 11){
+        setCommandCounter(2);
         return "Error:Network exists";
-    }else if(SIMcore.getCommandCounter() == 12){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 12){
+        setCommandCounter(2);
         return "Error:WIFI information error";
     }else{
         return "Error";
@@ -309,29 +309,29 @@ char*   DFRobot_L218::getLongitude(void)
 
 char*   DFRobot_L218::getLatitude(void)
 {
-    if((SIMcore.getCommandCounter() == 4) || (SIMcore.getCommandCounter() == 5)){
-        SIMcore.setCommandCounter(5);
+    if((getCommandCounter() == 4) || (getCommandCounter() == 5)){
+        setCommandCounter(5);
         return latitude;
-    }else if(SIMcore.getCommandCounter() == 6 ){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 6 ){
+        setCommandCounter(2);
         return "Error:Network busy";
-    }else if(SIMcore.getCommandCounter() == 7 ){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 7 ){
+        setCommandCounter(2);
         return "Error:Not ready";
-    }else if(SIMcore.getCommandCounter() == 8 ){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 8 ){
+        setCommandCounter(2);
         return "Error:Network error";
-    }else if(SIMcore.getCommandCounter() == 9 ){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 9 ){
+        setCommandCounter(2);
         return "Error:Timeout";
-    }else if(SIMcore.getCommandCounter() == 10){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 10){
+        setCommandCounter(2);
         return "Error:Unack";
-    }else if(SIMcore.getCommandCounter() == 11){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 11){
+        setCommandCounter(2);
         return "Error:Network exists";
-    }else if(SIMcore.getCommandCounter() == 12){
-        SIMcore.setCommandCounter(2);
+    }else if(getCommandCounter() == 12){
+        setCommandCounter(2);
         return "Error:WIFI information error";
     }else{
         return "Error";
